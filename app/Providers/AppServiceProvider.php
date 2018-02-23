2ded64f433ee3e410171e7018ae48e1a566892ee
polo dev <polodev10@gmail.com>
2018-02-23 15:58:28 +0600

    added database schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Schema;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
